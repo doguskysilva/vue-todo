a9c00d66c3f207c939aa06c10070eee935036a9f
Douglas Silva <dogusky.silva@gmail.com>
2019-01-21 15:23:04 -0200

    Welcome to user
    Add to index.html the box to get the name of user and show the name of user.
    Add to main.js the method to create user account and model to user.
    Add to main.css the fade effect.

--- assets/main.js
+++ assets/main.js
@@ -1,84 +1,108 @@
 Vue.component('todo-item', {
-    props: {
-        task: {}
+  props: {
+    task: {}
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+  data() {
+    return {
+      is_edit: false,
+      old_text: '',
+      new_text: ''
+    }
+  },
+  methods: {
+    edit() {
+      this.is_edit = true
+      this.new_text = this.task.text
+      this.$refs.task_edit.focus()
     },
-    directives: {
-        focus: {
-          inserted: function (el) {
-            el.focus()
-          }
-        }
+    cancel() {
+      this.is_edit = false
     },
-    data() {
-        return {
-            is_edit: false,
-            old_text: '',
-            new_text: ''
-        }
+    save() {
+      this.task.text = this.new_text
+      this.is_edit = false
+      this.$emit('update')
     },
-    methods: {
-        edit () {
-            this.is_edit = true
-            this.new_text = this.task.text
-            this.$refs.task_edit.focus()
-        },
-        cancel () {
-            this.is_edit = false
-        },
-        save () {
-            this.task.text = this.new_text
-            this.is_edit = false
-            this.$emit('update')
-        },
-        remove () {
-            this.$emit('remove', this.task.id)
-        }
-    },
-    template: '#todo-item'
+    remove() {
+      this.$emit('remove', this.task.id)
+    }
+  },
+  template: '#todo-item'
 });
 
 var app = new Vue({
-    el: '#app',
-    data() {
-        return {
-            task: {
-                id: '',
-                text: '',
-            },
-            tasks: [],
-            apple: 'akssks'
-        }
+  el: '#app',
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+  data() {
+    return {
+      user: {
+        name: null,
+        active: false
+      },
+      task: {
+        id: '',
+        text: '',
+      },
+      tasks: [],
+    }
+  },
+  methods: {
+    createAccount() {
+      if (!!this.user.name) {
+        this.user.active = true
+        localStorage.setItem('user', JSON.stringify(this.user))
+      }
     },
-    methods: {
-        remove (id) {
-            this.tasks = this.tasks.filter(x => x.id !== id)
-            this.sync_storage()
-        },
-        update () {
-            this.sync_storage()
-        },
-        save () {
-            if (!this.task.text == '') {
-                let id = Math.random().toString(36).substring(7)
-                this.task.id = id
-                this.tasks.push(Object.assign({}, this.task))
+    remove(id) {
+      this.tasks = this.tasks.filter(x => x.id !== id)
+      this.sync_storage()
+    },
+    update() {
+      this.sync_storage()
+    },
+    save() {
+      if (!this.task.text == '') {
+        let id = Math.random().toString(36).substring(7)
+        this.task.id = id
+        this.tasks.push(Object.assign({}, this.task))
 
-                this.task.id = ''
-                this.task.text = ''
+        this.task.id = ''
+        this.task.text = ''
 
-                this.sync_storage()
-            }
-        },
-        sync_storage () {
-            localStorage.setItem('tasks', JSON.stringify(this.tasks))
-        }
+        this.sync_storage()
+      }
     },
-    mounted() {
-        let tasks = localStorage.getItem('tasks')
-        if (tasks == null) {
-            localStorage.setItem('tasks', JSON.stringify([]))
-        } else {
-            this.tasks = JSON.parse(tasks)
-        }
-    },
+    sync_storage() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    }
+  },
+  mounted() {
+    let user = localStorage.getItem('user')
+    let tasks = localStorage.getItem('tasks')
+
+    if (user) {
+      this.user = JSON.parse(user)
+    }
+
+
+    // }
+    // if (!tasks) {
+    //     localStorage.setItem('tasks', JSON.stringify([]))
+    // } else {
+    //     this.tasks = JSON.parse(tasks)
+    // }
+  },
 });
